9a3ea72e4659403633bac728fc9742fee1c6689c
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2015-11-14 00:49:50 +0000

    Bones of a CGraph class

---
+++ src/CGraph.h
@@ -0,0 +1,31 @@
+/*
+ * CGraph.h
+ *
+ *  Created on: 13 Nov 2015
+ *      Author: jack
+ */
+
+#ifndef SRC_CGRAPH_H_
+#define SRC_CGRAPH_H_
+
+#include "../lib/Matrix.h"
+
+using namespace std;
+
+class CGraph
+{
+public:
+   // Constructors and destructors
+   CGraph(Numeric_lib::Matrix<bool, 2> adjacencyMatrix, Numeric_lib::Matrix<double, 2> distanceMatrix);
+   CGraph(Numeric_lib::Matrix<double, 2> distanceMatrix);
+   virtual ~CGraph();
+
+   // Non-modifying functions
+   void Dijkstra(const int& startVertex, vector<double>&, vector<vector<int>>&);
+
+private:
+   Numeric_lib::Matrix<bool, 2> m_AdjacencyMatrix;
+   Numeric_lib::Matrix<double, 2> m_DistanceMatrix;
+};
+
+#endif /* SRC_CGRAPH_H_ */
---
+++ src/CGraph_test.cpp
@@ -0,0 +1,10 @@
+/*
+ * CGraph_test.cpp
+ *
+ *  Created on: 13 Nov 2015
+ *      Author: jack
+ */
+
+#include "CGraph.h"
+
+// TODO Write test

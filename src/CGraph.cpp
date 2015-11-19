9e8f29eaca5e7db912c3f557c928176b034d41ea
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2015-11-19 22:52:54 +0000

    Removed dependence on Matrix class.
    Unfortunately this code won't compile - I'm not initialising the
    vector<vector<int> > m_AdjacencyMatrix correctly.
    Can anyone help?!

--- src/CGraph.cpp
+++ src/CGraph.cpp
@@ -3,51 +3,73 @@
  *
  *  Created on: 13 Nov 2015
  *      Author: Jack Buckingham
-*/
+ */
 
-//#include "CGraph.h"
-//
-//CGraph::CGraph(const Numeric_lib::Matrix<bool, 2>& adjacencyMatrix,
-//		const Numeric_lib::Matrix<double, 2>& distanceMatrix)
-//{
-//	m_AdjacencyMatrix{adjacencyMatrix};
-//	m_DistanceMatrix{distanceMatrix};
-//
-//	//	TODO Implement check that adjacency matrix is consistent with distance matrix
-//}
-//
-//CGraph::CGraph(const Numeric_lib::Matrix<double, 2>& distanceMatrix)
-//{
-//	m_DistanceMatrix { distanceMatrix };
-//
-//	int d1 = distanceMatrix.dim1();
-//	int d2 = distanceMatrix.dim2();
-//	Numeric_lib::Matrix<int, 2> adjacencyMatrix_tmp(d1, d2);
-//	for (int i = 0; i < d1; ++i)
-//	{
-//		for (int j = 0; j < d2; ++j)
-//		{
-//			if (m_DistanceMatrix(i, j) >= 0)
-//			{
-//				adjacencyMatrix_tmp(i, j) = 1;
-//			}
-//			else
-//			{
-//				adjacencyMatrix_tmp(i, j) = 0;
-//			}
-//		}
-//	}
-//
-//	m_AdjacencyMatrix { adjacencyMatrix_tmp };
-//}
-//
-//CGraph::~CGraph()
-//{
-//	// TODO Auto-generated destructor stub
-//}
-//
-//void CGraph::Dijkstra(const int& startVertex, vector<double>& rShortestDistances, vector<vector<int> >& rOutputRoutes)
-//{
-//	// TODO Implement Dijkstra
-//}
+// ~~~ INCLUDES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#include "CGraph.h"
+
+/* ~~~ FUNCTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * This function is a constructor for the CGraph class. It takes inputs for the distance matrix.
+ *
+ * It checks that
+ *   - The vector<vector<double> > is a matrix and is square.
+ *   - The entries of the matrix are >= 0 or -1 (-1 is used to represent infinity).
+ *
+ * It also initialises the adjacency matrix using the distance matrix.
+ *
+ */
+
+CGraph::CGraph(const vector<vector<double> >& distanceMatrix)
+		: m_DistanceMatrix { distanceMatrix }
+{
+	// Get dimensions and check the input is a matrix and is square
+	int& d = distanceMatrix.size();
+	for (int i = 0; i < d; ++i)
+	{
+		if (d != distanceMatrix[i].size())
+		{
+			throw InputDistMat_NotSquareMatrix { distanceMatrix };
+		}
+	}
+
+	// Check elements are valid
+	for (int i = 0; i < d; ++i)
+	{
+		for (int j = 0; j < d; ++j)
+		{
+			if (distanceMatrix[i][j] < 0 && distanceMatrix[i][j] != -1)
+			{
+				throw InputDistMat_InvalidElements { distanceMatrix };
+			}
+		}
+	}
+
+	// Create adjacency matrix
+	m_AdjacencyMatrix = vector(d);
+	for (int i = 0; i < d; ++i)
+	{
+		m_AdjacencyMatrix[i] = vector(d);
+		for (int j = 0; j < d; ++j)
+		{
+			if (distanceMatrix[i][j] >= 0)
+			{
+				m_AdjacencyMatrix[i][j] = 1;
+			}
+			else
+			{
+				m_AdjacencyMatrix[i][j] = 0;
+			}
+		}
+	}
+}
+
+CGraph::~CGraph()
+{
+// TODO Auto-generated destructor stub
+}
+
+void CGraph::Dijkstra(const int& startVertex, vector<double>& rShortestDistances, vector<vector<int> >& rOutputRoutes)
+{
+// TODO Implement Dijkstra
+}
 
--- src/CGraph.h
+++ src/CGraph.h
@@ -1,34 +1,65 @@
-
 /*
  * CGraph.h
  *
  *  Created on: 13 Nov 2015
  *      Author: jack
-*/
+ */
 
-//#ifndef SRC_CGRAPH_H_
-//#define SRC_CGRAPH_H_
-//
-//#include "../lib/Matrix.h"
-//#include<vector>
-//
-//using namespace std;
-//
-//class CGraph
-//{
-//public:
-//	// Constructors and destructors
-//	CGraph(const Numeric_lib::Matrix<bool, 2>& adjacencyMatrix, const Numeric_lib::Matrix<double, 2>& distanceMatrix);
-//	CGraph(const Numeric_lib::Matrix<double, 2>& distanceMatrix);
-//	virtual ~CGraph();
-//
-//	// Non-modifying functions
-//	void Dijkstra(const int& startVertex, vector<double>&, vector<vector<int> >&);
-//
-//private:
-//	Numeric_lib::Matrix<bool, 2> m_AdjacencyMatrix;
-//	Numeric_lib::Matrix<double, 2> m_DistanceMatrix;
-//};
-//
-//#endif  SRC_CGRAPH_H_
+#ifndef SRC_CGRAPH_H_
+#define SRC_CGRAPH_H_
+
+#include<vector>
+
+using namespace std;
+
+/* ~~~ CLASS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * This is a class to represent the mathematical concept of a graph.
+ * See https://en.wikipedia.org/wiki/Graph_theory
+ *
+ * Member functions:
+ *  - Dijkstra = An implementation of Dijkstra's algorithm.
+ *               https://en.wikipedia.org/wiki/Dijkstra's_algorithm
+ *
+ * Member variables:
+ *  - m_distance matrix  = A distance matrix for the graph. Use -1 to indicate an infinite distance.
+ *  - m_adjacency matrix = An adjacency matrix for the graph. This should be kept consistent with
+ *                         distance matrix.
+ *
+ *
+ */
+class CGraph
+{
+public:
+	// Exceptions
+	class InputDistMat_NotSquareMatrix
+	{
+		vector<vector<double> > mm_DistanceMatrix;
+		InputDistMat_NotSquareMatrix(vector<vector<double> > distanceMatrix)
+				: mm_DistanceMatrix { distanceMatrix }
+		{
+		}
+	};
+	class InputDistMat_InvalidElements
+	{
+		vector<vector<double> > mm_DistanceMatrix;
+		InputDistMat_InvalidElements(vector<vector<double> > distanceMatrix)
+				: mm_DistanceMatrix
+						{ distanceMatrix }
+		{
+		}
+	};
+
+	// Constructors and destructors
+	CGraph(const vector<vector<double> >& distanceMatrix);
+	virtual ~CGraph();
+
+	// Non-modifying functions
+	void Dijkstra(const int& startVertex, vector<double>&, vector<vector<int> >&);
+
+private:
+	vector<vector<int> > m_AdjacencyMatrix;
+	vector<vector<double> > m_DistanceMatrix;
+};
+
+#endif // SRC_CGRAPH_H_
 

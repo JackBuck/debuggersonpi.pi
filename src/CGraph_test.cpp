8aec65c8207d4cdbf85aec18411b65461932ecba
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2015-12-24 00:42:26 +0000

    Updated CGraph_test

--- src/CGraph_test.cpp
+++ src/CGraph_test.cpp
@@ -11,6 +11,14 @@
 
 using namespace std;
 
+// For printing elements of a table
+template<typename T> void printElement(T t, const int& width)
+{
+	const char separator = ' ';
+	cout << right << setw(width) << setfill(separator) << t;
+}
+
+// Test CGraph, in particular the Dijkstra algorithm
 int CGraph_test()
 {
 	/*
@@ -36,56 +44,73 @@ int CGraph_test()
 	};
 
 	CGraph exampleGraph { exampleGraph_DistMat };
-	int exampleGraphOrder = 7;
 
 	// 2 - Define hand computed result of Dijkstra manually
-	// Vertex   Parent              Shortest Distance
-	// 0        n/a (start vertex)  0
-	// 1        0                   1
-	// 2        1                   2
-	// 3        2                   3
-	// 4        2                   3
-	// 5        6                   3
-	// 6        2                   2
-	vector<int> expected_outputRoutes {0, 0, 1, 2, 2, 6, 2};
-	vector<double> expected_shortestDistances {0, 1, 2, 3, 3, 3, 2};
+	// TODO: Load these test cases from a file?
+	vector<unsigned int> startVertices        { 0, 0, 0, 3, 3, 0 };
+	vector<unsigned int> endVertices          { 0, 3, 5, 0, 6, 3 };
+	vector<double> expected_shortestDistances { 0, 3, 3, 3, 1, 3 };
+	vector<vector<unsigned int> > expected_outputRoutes {
+			{ 0 },
+			{ 0, 1, 2, 3 },
+			{ 0, 1, 2, 6, 5 },
+			{ 3, 2, 1, 0 },
+			{ 3, 2, 6 },
+			{ 0, 1, 2, 3 }
+	};
+	int numTestCases = startVertices.size();
 
 	// 3 - Call Dijkstra function in CGraph class
-	// TODO Update to use the new Dijkstra
-	vector<double> shortestDistances;
-	vector<int> outputRoutes;
-	exampleGraph.Dijkstra(0, shortestDistances, outputRoutes);
+	vector<double> shortestDistances(numTestCases);
+	vector<vector<unsigned int> > outputRoutes(numTestCases);
+	for (int i = 0; i < numTestCases; ++i)
+		exampleGraph.ShortestDistance(startVertices[i], endVertices[i], shortestDistances[i], outputRoutes[i]);
 
 	// 4 - Compare output with expected output
 	bool success = true;
-	for (int i = 0; i < exampleGraphOrder; ++i)
-	{
-		if (expected_outputRoutes[i] != outputRoutes[i])
-			success = false;
-		if (expected_shortestDistances[i] != shortestDistances[i])
-			success = false;
-	}
+	if (expected_outputRoutes != outputRoutes)
+		success = false;
+	if (expected_shortestDistances != shortestDistances)
+		success = false;
 
-	// Display output
+	// -Display output-
+	// TODO: Learn how to format strings in order to nicely display expected values in brackets as well
 	cout << "--CGraph_test--\n\n";
-	cout << "Output Routes:\n";
-	cout << setw(10) << "Expected  ";
-	for (int i = 0; i < exampleGraphOrder; ++i)
-		cout << setw(3) << expected_outputRoutes[i];
-	cout << '\n' << setw(10) << "Received  ";
-	for (int i = 0; i < exampleGraphOrder; ++i)
-		cout << setw(3) << outputRoutes[i];
+
+	const int colWidth_startVertex      = 12;
+	const int colWidth_endVertex        = 10;
+	const int colWidth_shortestDistance = 17;
+	const string columnSeparator = "   ";
+
+	// Column headers
+	printElement("Start Vertex", colWidth_startVertex);
+	cout << columnSeparator;
+	printElement("End Vertex", colWidth_endVertex);
+	cout << columnSeparator;
+	printElement("Shortest Distance", colWidth_shortestDistance);
+	cout << columnSeparator;
+	cout << "Output Routes" << '\n';
+
+	// Table contents
+	for (int i = 0; i < numTestCases; ++i)
+	{
+		printElement(startVertices[i], colWidth_startVertex);
+		cout << columnSeparator;
+		printElement(endVertices[i], colWidth_endVertex);
+		cout << columnSeparator;
+		printElement(shortestDistances[i], colWidth_shortestDistance);
+		cout << columnSeparator;
+		for (int j = 0; j < (int) outputRoutes[i].size(); ++j)
+			cout << outputRoutes[i][j] << ' ';
+		cout << " (";
+		for (int j = 0; j < (int) expected_outputRoutes[i].size(); ++j)
+			cout << expected_outputRoutes[i][j] << ' ';
+		cout << ')';
+		cout << '\n';
+	}
 	cout << '\n';
 
-	cout << "Shortest Distances:\n";
-		cout << setw(10) << "Expected  ";
-		for (int i = 0; i < exampleGraphOrder; ++i)
-			cout << setw(3) << expected_shortestDistances[i];
-		cout << '\n' << setw(10) << "Received  ";
-		for (int i = 0; i < exampleGraphOrder; ++i)
-			cout << setw(3) << shortestDistances[i];
-	cout << '\n';
-
+	// Return success or failure
 	if (success)
 	{
 		cout << "\nSuccess!\n";

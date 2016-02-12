ba95db730542aa856bff4f44093c8e69a081ed11
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2016-02-12 19:26:13 +0000

    Modified CGraph to cope with singleton and disconnected graphs.

--- src/CGraph.cpp
+++ src/CGraph.cpp
@@ -193,6 +193,10 @@ void CGraph::ExternalToInternal(const vector<int>& vertices_external, vector<uns
  * It returns the shortest distance between two specified vertices, and an example shortest route
  * between them. This is done by searching for a previous appropriate set of Dijkstra outputs, and
  * calling Dijkstra if none exists.
+ *
+ * If the two specified vertices are not connected then the shortest distance is returned as -1 and
+ * the route between them is empty.
+ *
  * Inputs and outputs use the external vertex numbering.
  *
  * INPUTS:
@@ -207,11 +211,14 @@ void CGraph::ExternalToInternal(const vector<int>& vertices_external, vector<uns
  *                     this is usually what the caller will want).
  *
  * INPUT OUTPUTS:
- * shortestDistance = The shortest distance between startVertex and endVertex.
+ * shortestDistance = The shortest distance between startVertex and endVertex. If startVertex and
+ *                    endVertex are not connected then shortestDistance is returned -1.
  * outputRoute      = A vector containing an example shortest route from startVertex to endVertex.
  *                    outputRoute.front() == startVertex
  *                    outputRoute[i] == the ith element of the route
  *                    outputRoute.back() == endVertex
+ *                    If startVertex and endVertex are not connected then outputRoute is an empty
+ *                    vector.
  *
  */
 double CGraph::ShortestDistance(const int& startVertex, const int& endVertex, const bool& preferStartVertex, vector<int>& outputRoute)
@@ -300,20 +307,23 @@ void CGraph::InternalShortestDistance(const unsigned int& startVertex, const uns
 		// corresponding to startVertex
 		unsigned int index = m_DijkstraStartVertices[startVertex];
 
-		// Set first element in reverseOutputRoute as endVertex.
-		// Then repeatedly push_back the parent vertex of the latest vertex in reverseOutputRoute until we reach startVertex
-		vector<unsigned int> reverseOutputRoute = {endVertex};
-		while (reverseOutputRoute.back() != startVertex)
-			reverseOutputRoute.push_back(m_DijkstraOutputRoutes[index][reverseOutputRoute.back()]);
-
-		// Set outputRoute as reverse of reverseOutputRoute
-		outputRoute = vector<unsigned int> (reverseOutputRoute.rbegin(), reverseOutputRoute.rend());
-//		outputRoute = vector<unsigned int> (reverseOutputRoute.size());
-//		for (unsigned int i = 0; i < reverseOutputRoute.size(); ++i)
-//			outputRoute[i] = reverseOutputRoute[reverseOutputRoute.size()-1 - i];
-
-		// Set shortestDistance
+		// Set the shortestDistance
 		shortestDistance = m_DijkstraShortestDistances[index][endVertex];
+
+		// Set the outputRoute
+		if (shortestDistance == -1) // If startVertex and endVertex are not connected
+			outputRoute = vector<unsigned int> ();
+		else // If startVertex and endVertex are connected
+		{
+			// Set first element in reverseOutputRoute as endVertex.
+			// Then repeatedly push_back the parent vertex of the latest vertex in reverseOutputRoute until we reach startVertex
+			vector<unsigned int> reverseOutputRoute = {endVertex};
+			while (reverseOutputRoute.back() != startVertex)
+				reverseOutputRoute.push_back(m_DijkstraOutputRoutes[index][reverseOutputRoute.back()]);
+
+			// Set outputRoute as reverse of reverseOutputRoute
+			outputRoute = vector<unsigned int> (reverseOutputRoute.rbegin(), reverseOutputRoute.rend());
+		}
 	}
 	else
 	{
@@ -321,14 +331,20 @@ void CGraph::InternalShortestDistance(const unsigned int& startVertex, const uns
 		// corresponding to endVertex
 		unsigned int index = m_DijkstraStartVertices[endVertex];
 
-		// Set first element in outputRoute as startVertex
-		// Then repeatedly push_back the parent vertex of the latest vertex in outputRoute until we reach endVertex
-		outputRoute = vector<unsigned int> {startVertex};
-		while (outputRoute.back() != endVertex)
-			outputRoute.push_back(m_DijkstraOutputRoutes[index][outputRoute.back()]);
-
 		// Set shortestDistance
 		shortestDistance = m_DijkstraShortestDistances[index][startVertex];
+
+		// Set the outputRoute
+		if (shortestDistance == -1)
+			outputRoute = vector<unsigned int> ();
+		else
+		{
+			// Set first element in outputRoute as startVertex
+			// Then repeatedly push_back the parent vertex of the latest vertex in outputRoute until we reach endVertex
+			outputRoute = vector<unsigned int> {startVertex};
+			while (outputRoute.back() != endVertex)
+				outputRoute.push_back(m_DijkstraOutputRoutes[index][outputRoute.back()]);
+		}
 	}
 
 }
@@ -352,18 +368,18 @@ void CGraph::InternalShortestDistance(const unsigned int& startVertex, const uns
  * 	startVertex.
  *
  * MEMBER VARIABLES SET:
- * 	m_DijkstraShortestDistances - A vector of integers will be created which contains the shortest
+ * 	m_DijkstraShortestDistances - A vector of doubles will be created which contains the shortest
  * 	              distances from the startVertex to each vertex of the graph. A value of -1 will
  * 	              be used to indicate that the vertex is not connected to startVertex. This vector
  * 	              is then appended to m_DijkstraShortestDistances.
- * 	m_DijkstraOutputRoutes - A vector of integers will be created to define examples of paths from
- * 	              the startVertex to each vertex of the graph which have the shortest total
- * 	              distance. The union of the example paths form a tree which is defined here by
- * 	              setting outputRoutes[i] to be the 'parent' of vertex i in the tree. By default,
- * 	              outputRoutes[startVertex] = startVertex. If a vertex i is not part of the same
- * 	              connected component as startVertex then outputRoutes[i] is -1.
- * 	              This vector is then appended to m_DijkstraOutputRoutes.
- * 	m_DijkstraStartVertices - A key-value pair is added in the map<int,int>
+ * 	m_DijkstraOutputRoutes - A vector ofunsigend integers will be created to define examples of
+ * 	              paths from the startVertex to each vertex of the graph which have the shortest
+ * 	              total distance. The union of the example paths form a tree which is defined here
+ * 	              by setting outputRoutes[i] to be the 'parent' of vertex i in the tree. By
+ * 	              default, outputRoutes[startVertex] = startVertex. If a vertex i is not part of
+ * 	              the same connected component as startVertex then outputRoutes[i] is -1. This
+ * 	              vector is then appended to m_DijkstraOutputRoutes.
+ * 	m_DijkstraStartVertices - A key-value pair is added in the map<unsigned int, unsigned int>
  * 	              m_DijkstraStartVertices to save the start vertex used to generate the Dijkstra
  * 	              results. The key is the startVertex and the value is the corresponding index in
  * 	              m_DijkstraShortestDistances and m_DijkstraOutputRoutes.
@@ -379,7 +395,7 @@ unsigned int CGraph::InternalDijkstra(const unsigned int& startVertex)
 
 	// Create and initialise vectors shortestDistances and outputRoutes
 	vector<double> shortestDistances(m_Order, -1);
-	vector<unsigned int> outputRoutes(m_Order, -1);
+	vector<unsigned int> outputRoutes(m_Order, -1); // (i.e. largest unsigned integer possible)
 	for (unsigned int i = 0; i < m_Order; ++i)
 	{
 		if (i == startVertex || m_AdjacencyMatrix[startVertex][i])
@@ -388,18 +404,16 @@ unsigned int CGraph::InternalDijkstra(const unsigned int& startVertex)
 			shortestDistances[i] = m_DistanceMatrix[startVertex][i];
 		}
 	}
-	outputRoutes[startVertex] = startVertex;
 
 	// Initialise knownDistances
 	vector<bool> knownDistances(m_Order, false);
 	knownDistances[startVertex] = true;
 
 	// -- Main Algorithm Body -- //
-	bool moreVertices = true; // If graph is empty then startVertex will not be a valid vertex and an exception will already have been thrown
-	while (moreVertices)
+	while (true)
 	{
 		// Find the vertex with the shortest unconfirmed shortestDistance to startVertex
-		unsigned int nextClosest = -1;
+		unsigned int nextClosest = -1; // (i.e. largest unsigned integer possible)
 		double nextShortestDistance = -1;
 		for (unsigned int i = 0; i < m_Order; ++i)
 		{
@@ -413,6 +427,10 @@ unsigned int CGraph::InternalDijkstra(const unsigned int& startVertex)
 				nextShortestDistance = shortestDistances[i];
 			}
 		}
+
+		// If for every vertex, either we know its shortest distance to startVertex, or it is not connected to such a vertex,
+		// then stop the algorithm.
+		if (nextClosest == (unsigned)-1) break;
 
 		// Update shortest distances and shortest paths
 		for (unsigned int i = 0; i < m_Order; ++i)
@@ -432,14 +450,6 @@ unsigned int CGraph::InternalDijkstra(const unsigned int& startVertex)
 		// Update knownDistances
 		knownDistances[nextClosest] = true;
 
-		// Check whether there are still more
-		// (if remaining vertices are all infinity then don't bother checking any more)
-		moreVertices = false;
-		for (unsigned int i = 0; i < m_Order; ++i)
-		{
-			if (!knownDistances[i] && shortestDistances[i] != -1)
-				moreVertices = true;
-		}
 	}
 
 	// -- Save Results in Member Variables -- //

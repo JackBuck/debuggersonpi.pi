3b2986e37dc91a259c1e9007f0956ef3dcede689
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2016-02-20 02:38:34 +0000

    Reverted catastrophic CGraph errors introduced to Master by John in d8b3e9d...
    The block attached to two if statements had been commented, meaning the if condition then applied to the following statement instead!

--- src/CGraph.cpp
+++ src/CGraph.cpp
@@ -58,7 +58,7 @@ CGraph::CGraph(const vector<vector<double> > &distanceMatrix, const vector<int>&
 
 	switch (distanceMatrixType) {
 	case DistMatCheckResult::tooLarge:
-		//throw InputDistMat_MatrixTooLarge { distanceMatrix.size(), std::numeric_limits<unsigned int>::max() - 1 };
+		throw InputDistMat_MatrixTooLarge { distanceMatrix.size(), std::numeric_limits<unsigned int>::max() - 1 };
 		break;
 	case DistMatCheckResult::badShape:
 		throw InputDistMat_BadShape{ distanceMatrix };
@@ -103,7 +103,7 @@ CGraph::CGraph(const vector<vector<double> > &distanceMatrix, const vector<int>&
 
 	// Check and store external-to-internal vertex labels
 	if (m_Order != vertexLabels.size())
-		//throw InputVertexLabels_BadSize { vertexLabels.size() };
+		throw InputVertexLabels_BadSize { vertexLabels.size() };
 	for (unsigned int i = 0; i < vertexLabels.size(); ++i)
 		m_ExternalToInternal[vertexLabels[i]] = i;
 	if (m_Order != m_ExternalToInternal.size())

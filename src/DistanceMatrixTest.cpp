a54b4ca5f73dd06f873c18173a98f34e5c86267c
John Connolly <jc140593@renishaw.com>
2016-02-23 17:53:39 +0000

    working DistanceMatrix

--- src/DistanceMatrixTest.cpp
+++ src/DistanceMatrixTest.cpp
@@ -4,53 +4,53 @@
 #include "DebugLog.hpp"
 
 
-int CMap_test()
-{
-	DEBUG_METHOD();
-
-
-	/////////////////////////////////////////////////////////////
-	// Create instance of CMap 
-	std::string fileInputpath = "TestData/PracticeMap.csv";
-	CMap Maze(fileInputpath);
-
-	/////////////////////////////////////////////////////////////
-	//
-	std::ofstream myfile;
- 	myfile.open("TestData/example.txt", std::fstream::out);
-	if (myfile.is_open())
-	{
-		std::cout << "--CMap_test--\n\n";
-
-		std::vector<std::vector<ERoom>> roomMap = Maze.GetRoomMap();
-
-		Maze.UpdateCellMap();
-
-		std::vector<std::vector<int>> cellMap = Maze.GetCellMap();
-
-		for (unsigned int i = 0; i<cellMap.size(); i++)
-		{
-			for (unsigned int j = 0; j<cellMap[i].size(); j++)
-			{
-				std::cout << cellMap[i][j];
-			}
-			std::cout << std::endl;
-		}
-
-
-	}
-	else
-	{
-		std::cout << "Unable to open file \n";
-		return 1;
-	}
-
-	myfile.close();
-
-	std::string fileOutputPath = "TestData/5x5testmap1Output.txt";
-	Maze.WriteCellMap(fileOutputPath);
-	return 0;
-}
+//int DistanceMatrixTest()
+//{
+//	DEBUG_METHOD();
+//
+//
+//	/////////////////////////////////////////////////////////////
+//	// Create instance of CMap 
+//	std::string fileInputpath = "TestData/5x5testmap1.txt";
+//	CMap Maze(fileInputpath);
+//
+//	/////////////////////////////////////////////////////////////
+//	//
+//	std::ofstream myfile;
+//	myfile.open("TestData/example.txt", std::fstream::out);
+//	if (myfile.is_open())
+//	{
+//		std::cout << "--CMap_test--\n\n";
+//
+//		std::vector<std::vector<ERoom>> roomMap = Maze.GetRoomMap();
+//
+//		Maze.UpdateCellMap();
+//
+//		std::vector<std::vector<int>> cellMap = Maze.GetCellMap();
+//
+//		for (unsigned int i = 0; i<cellMap.size(); i++)
+//		{
+//			for (unsigned int j = 0; j<cellMap[i].size(); j++)
+//			{
+//				std::cout << cellMap[i][j];
+//			}
+//			std::cout << std::endl;
+//		}
+//
+//
+//	}
+//	else
+//	{
+//		std::cout << "Unable to open file \n";
+//		return 1;
+//	}
+//
+//	myfile.close();
+//
+//	std::string fileOutputPath = "TestData/5x5testmap1Output.txt";
+//	Maze.WriteCellMap(fileOutputPath);
+//	return 0;
+//}
 //
 //void PrintMatrix(std::vector<std::vector<int>> inputMap)
 //{

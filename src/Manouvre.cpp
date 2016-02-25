b6a195a8f0bc25e7b7e9b399f3a811265ccae065
Jack Buckingham <jack_buckingham@blueyonder.co.uk>
2016-02-25 23:55:24 +0000

    Flagged defect in code with comment

--- src/Manouvre.cpp
+++ src/Manouvre.cpp
@@ -97,7 +97,7 @@ void CManouvre::StraightAcrossRoomJunct()
 	DEBUG_METHOD();
 
 	CGoodsOut::ForwardDetectJunction();
-	CGoodsOut::ForwardDetectJunction();
+	CGoodsOut::ForwardDetectJunction(); // TODO: This is wrong!!!
 }
 
 void CManouvre::StraightAcrossRoomNoJunct()
